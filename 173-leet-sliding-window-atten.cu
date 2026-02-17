624c6ae77f5261c1270eec474f8a9861de0ad344
JackeyLove1 <1572727410@qq.com>
2026-02-17 08:37:31 +0800

    some codes

--- 173-leet-sliding-window-atten.cu
+++ 173-leet-sliding-window-atten.cu
@@ -176,6 +176,16 @@ __forceinline__ __device__ T BlockReduceDynamic(T value, ReduceOpType op_type) {
 static constexpr int MATMUL_TILE = 16;
 [[maybe_unused]] static constexpr float DEFAULT_ALIBI_ALPHA = 1.0f;
 
+#define CUDA_CHECK(call)                                               \
+  do {                                                                 \
+    cudaError_t _status = (call);                                      \
+    if (_status != cudaSuccess) {                                      \
+      fprintf(stderr, "CUDA error at %s:%d: %s\n", __FILE__, __LINE__, \
+              cudaGetErrorString(_status));                            \
+      return;                                                          \
+    }                                                                  \
+  } while (0)
+
 /**
 1. Standard Softmax Attention
 Given query matrix Q, key matrix K, and value matrix V, each position i attends to all positions j
@@ -191,5 +201,127 @@ scores to obtain attention weights. ·Use the weights to compute a weighted aver
 the same window: outputᵢ= ∑ⱼ∈[i-window size,i+window size]softmax(scoreᵢⱼ*)ⱼ⋅Vⱼ
  */
 
-extern "C" void solve(const float* Q, const float* K, const float* V, float* output, int M, int d,
-                      int window_size) {}
+// A @ B.T , A[M, N] B[K, N] C[M, K]
+// threadPerBlock(TILE, TILE) blockPerGrid(CEIL(K, TILE), CEIL(M, TILE))
+template <int TILE>
+__global__ void matmul_kernel(const float* __restrict__ A, const float* __restrict__ B,
+                              float* __restrict__ c, const int M, const int N, const int K,
+                              const float scale) {
+  const int tx = threadIdx.x, ty = threadIdx.y;
+  const int row = blockIdx.y * TILE + ty;
+  const int col = blockIdx.x * TILE + tx;
+  float sum{0.0f};
+  const auto num_tiles = CEIL(N, TILE);
+  __shared__ __align__(16) float sh_A[2][TILE][TILE + 1];
+  __shared__ __align__(16) float sh_B[2][TILE][TILE + 1];
+
+  cuda::pipeline<cuda::thread_scope_thread> pipe = cuda::make_pipeline();
+
+  pipe.producer_acquire();
+  if (row < M && tx < N) {
+    cuda::memcpy_async(&sh_A[0][ty][tx], &A[row * N + tx], sizeof(float), pipe);
+  } else {
+    sh_A[0][ty][tx] = 0.0f;
+  }
+  if (col < K && ty < N) {
+    // B is [K, N], while this kernel computes A @ B.T.
+    // So B.T[ty, col] maps to B[col, ty].
+    cuda::memcpy_async(&sh_B[0][ty][tx], &B[col * N + ty], sizeof(float), pipe);
+  } else {
+    sh_B[0][ty][tx] = 0.0f;
+  }
+  pipe.producer_commit();
+
+  for (int k = 0; k < num_tiles; ++k) {
+    if (k + 1 < num_tiles) {
+      pipe.producer_acquire();
+      auto next_tile = k + 1;
+      auto write_stage = next_tile % 2;
+      const auto a_col = next_tile * TILE + tx;
+      const auto b_col = next_tile * TILE + ty;
+
+      if (row < M && a_col < N) {
+        cuda::memcpy_async(&sh_A[write_stage][ty][tx], &A[row * N + a_col], sizeof(float), pipe);
+      } else {
+        sh_A[write_stage][ty][tx] = 0.0f;
+      }
+      if (col < K && b_col < N) {
+        cuda::memcpy_async(&sh_B[write_stage][ty][tx], &B[col * N + b_col], sizeof(float), pipe);
+      } else {
+        sh_B[write_stage][ty][tx] = 0.0f;
+      }
+      pipe.producer_commit();
+    }
+
+    pipe.consumer_wait();
+    __syncthreads();
+
+    auto read_stage = k % 2;
+#pragma unroll
+    for (int kk = 0; kk < TILE; ++kk) {
+      sum += sh_A[read_stage][ty][kk] * sh_B[read_stage][kk][tx];
+    }
+    __syncthreads();
+    pipe.consumer_release();
+  }
+
+  if (row < M && col < K) {
+    c[row * K + col] = sum / scale;
+  }
+}
+
+__global__ __launch_bounds__(256) void row_softmax_kernel(float* __restrict__ A, const int M,
+                                                          const int N) {
+  const int tid = threadIdx.x;
+  const int row = blockIdx.x;
+  const int row_offset = row * N;
+  const auto nthreads = blockDim.x;
+  float max_val = -INFINITY;
+  float sum_val = 0.0f;
+  __shared__ float global_max;
+  __shared__ float global_sum;
+  for (int i = tid; i < N; i += nthreads) {
+    max_val = fmaxf(max_val, A[row_offset + i]);
+  }
+  max_val = BlockReduceDynamic(max_val, ReduceOpType::MAX);
+  if (tid == 0) {
+    global_max = max_val;
+  }
+  __syncthreads();
+
+  for (int i = tid; i < N; i += nthreads) {
+    sum_val += __expf(A[row_offset + i] - global_max);
+  }
+  sum_val = BlockReduceDynamic(sum_val, ReduceOpType::SUM);
+  if (tid == 0) {
+    global_sum = sum_val;
+  }
+  __syncthreads();
+
+  for (int i = tid; i < N; i += nthreads) {
+    A[row_offset + i] = __expf(A[row_offset + i] - global_max) / global_sum;
+  }
+}
+
+__global__ void slidw_window_attention() {}
+
+extern "C" void solve(const float* Q, const float* K, const float* V, float* output, const int M,
+                      const int d, const int window_size) {
+  float* scores;
+  CUDA_CHECK(cudaMalloc(&scores, sizeof(float) * M * M));
+  const dim3 matmul_block(MATMUL_TILE, MATMUL_TILE);
+  const dim3 qk_grid(CEIL(d, MATMUL_TILE), CEIL(M, MATMUL_TILE));
+  // scores[N, N] = (Q_i[N, d] @ K_i[N, d]^T) / sqrt(d)
+  const float scale = 1.0f / sqrtf(d);
+  matmul_kernel<MATMUL_TILE><<<qk_grid, matmul_block>>>(Q, K, scores, M, d, M, scale);
+  CUDA_CHECK(cudaGetLastError());
+
+  // scores[M, M] = softmax(scores[M, M])
+  row_softmax_kernel<<<M, 256>>>(scores, M, M);
+  CUDA_CHECK(cudaGetLastError());
+
+  // output[M, d] = scores[M, M] @ V[M, d]
+
+  CUDA_CHECK(cudaGetLastError());
+  CUDA_CHECK(cudaFree(scores));
+}
