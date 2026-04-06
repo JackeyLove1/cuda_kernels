88b6d0120eaf12cd0a24b2f52a5518412392ae88
JackeyLove1 <1572727410@qq.com>
2026-04-06 15:55:25 +0800

    feat: support bun and typescript

---
+++ 62.ts
@@ -0,0 +1,6 @@
+const nums = [1, 2, 3, 4, 5];
+const sum = nums.reduce((acc, value) => acc + value, 0);
+
+console.log("Hello from Bun + TypeScript");
+console.log("nums =", nums);
+console.log("sum =", sum);
